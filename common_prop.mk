4b230359d22ed8f4c31fc50727b13c3014a6a0a0
Markdarkness <mv10marcosmv@gmail.com>
2021-01-05 22:19:11 -0300

    sdm660-common: remove cabl cabl moved for device_wayne prop

--- common_prop.mk
+++ common_prop.mk
@@ -162,7 +162,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.hwc.enable_vds=1 \
     ro.opengles.version=196610 \
-    ro.qualcomm.cabl=0 \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
