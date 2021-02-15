fea50440f380afccbbe17ac07f5b0f7f8a96b1ef
Marcos Almeida <36458823+Markdarkness@users.noreply.github.com>
2021-02-15 20:00:39 -0300

    sdm660-common: Update ANT radio from Android 11

--- sdm660.mk
+++ sdm660.mk
@@ -127,12 +127,9 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
-
-# ANT Permission
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
+    libantradio \
+    antradio_app
+    
 # AuthSecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0-service
